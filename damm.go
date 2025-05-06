582e9c732eddcc67ff596d925a655b438207abe0
ww24 <ww24@users.noreply.github.com>
2025-05-07 02:09:08 +0900

    feat: add damm32, damm64

---
+++ damm.go
@@ -0,0 +1,53 @@
+package damm
+
+type Damm interface {
+	Generate(digits []int) int
+	Verify(digits []int) bool
+	Modulus() int
+}
+
+func calculate(digits []int, modulus, mask int) (checkDigit int) {
+	for _, digit := range digits {
+		checkDigit ^= digit
+		checkDigit <<= 1
+		if checkDigit >= modulus {
+			checkDigit ^= mask
+		}
+	}
+	return checkDigit
+}
+
+type damm struct {
+	modulus int
+	mask    int
+}
+
+func (d *damm) Generate(digits []int) int {
+	return calculate(digits, d.modulus, d.mask)
+}
+
+func (d *damm) Verify(digits []int) bool {
+	last := len(digits) - 1
+	checkDigit := calculate(digits[:last], d.modulus, d.mask)
+	return checkDigit == digits[last]
+}
+
+func (d *damm) Modulus() int {
+	return d.modulus
+}
+
+func New32() Damm {
+	const modulus = 32
+	return &damm{
+		modulus: modulus,
+		mask:    modulus | 5,
+	}
+}
+
+func New64() Damm {
+	const modulus = 64
+	return &damm{
+		modulus: modulus,
+		mask:    modulus | 3,
+	}
+}
